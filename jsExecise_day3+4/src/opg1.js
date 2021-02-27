3d16dc861001f0564d61899f766eb9994e291a3f
Peter-Rambeck <peter.rambeck@me.com>
2021-02-27 19:05:20 +0100

    Sat27

--- jsExecise_day3+4/src/opg1.js
+++ jsExecise_day3+4/src/opg1.js
@@ -1,46 +1,84 @@
 
 let boys = ["Peter", "Lars", "Ole"];
 let girls = ["Janne", "Hanne", "Sanne"];
-
-let all = boys.concat(girls);
-console.log("All: " + all);
-
-
-all.join(', ');
-console.log("c join: " + all );
+console.log(boys);
+console.log(girls);
 
 
-let x = all.join(' - ');
-console.log("d join: " + x);
+let concat = [...boys, ...girls];
+console.log(concat);
 
 
-//all.push(" Hanne", " Gitte");
-// console.log("e push: " + x);
+const join1 = concat.join(", ");
+console.log(join1);
+console.log(typeof(join1));
+
+concat.push("Hans");
+console.log(concat);
+
+// Shift remove 1st element
+concat.shift();
+console.log(concat);
+
+// POP remove last element
+concat.pop();
+console.log(concat);
+
+// Splice ( index, deleteCount )
+concat.splice(1, 2);
+console.log(concat);
 
 
-let newAll = ["Hans", " Kurt", ... all];
-console.log("e unshift: " + newAll);
+// concat.sort((a :string, b:String) => {return a.localCompare(b)}); 
+concat.sort( (a, b) =>  {return a.localeCompare(b)});
+console.log("Sort " + concat);
+console.log(typeof(concat));
 
-newAll.shift();
-console.log("g shift: " + newAll);
+console.log(concat.map((x) => x+"1" + x.length));
 
-newAll.pop();
-console.log("f pop: " + newAll);
-
-let z = new Array(newAll);
-newAll.splice(2, 1);
-console.log("g splice: " + newAll);
-
-newAll.reverse();
-console.log("h reverse: " + newAll);
-
-newAll.sort();
-console.log("i sort: " + newAll);
+console.log(concat.filter( (x) => x.length > 4));
 
 
-var f1 = function(n1,n2, callback){
-  return ": "+n1+" and "+n2+" = "+callback(n1,n2);
-};      
+//
+//let all = boys.concat(girls);
+//console.log("All: " + all);
+//
+//
+//all.join(', ');
+//console.log("c join: " + all );
+//
+//
+//let x = all.join(' - ');
+//console.log("d join: " + x);
+//
+//
+////all.push(" Hanne", " Gitte");
+//// console.log("e push: " + x);
+//
+//
+//let newAll = ["Hans", " Kurt", ... all];
+//console.log("e unshift: " + newAll);
+//
+//newAll.shift();
+//console.log("g shift: " + newAll);
+//
+//newAll.pop();
+//console.log("f pop: " + newAll);
+//
+//let z = new Array(newAll);
+//newAll.splice(2, 1);
+//console.log("g splice: " + newAll);
+//
+//newAll.reverse();
+//console.log("h reverse: " + newAll);
+//
+//newAll.sort();
+//console.log("i sort: " + newAll);
+//
+//
+//var f1 = function(n1,n2, callback){
+//  return ": "+n1+" and "+n2+" = "+callback(n1,n2);
+//};      
 
 
 
